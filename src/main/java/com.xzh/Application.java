b829d6a5589cbd94b9d5b530651cde3c8d409492
xzh <anenan@qq.com>
2022-05-11 16:38:33 +0800

    窗口优化

--- src/main/java/com.xzh/Application.java
+++ src/main/java/com.xzh/Application.java
@@ -1,6 +1,7 @@
 package com.xzh;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * @author 向振华
@@ -10,27 +11,17 @@ public class Application {
 
     public static void main(String[] args) {
 
-        // 窗口
-        JFrame frame = new JFrame();
-        // 设置窗口标题
-        frame.setTitle("窗口");
-        // 设置窗口显示尺寸
-        frame.setSize(400, 200);
-        // 设置窗口是否可见
-        frame.setVisible(true);
-        // 设置窗口是否可以关闭
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        // 面板
-        JPanel panel = new JPanel();
-        // 将面板添加到窗口
-        frame.add(panel);
-
         // 标签
         JLabel label = new JLabel();
         label.setText("标签");
-        // 将标签添加到面板
-        panel.add(label);
 
+        // 单行文本框
+        JTextField textField = new JTextField();
+        textField.setPreferredSize(new Dimension(200, 20));
+
+        // 按钮
+        JButton button = new JButton("按钮");
+
+        JFrameBuilder.init(label, textField, button);
     }
 }
---
+++ src/main/java/com.xzh/JFrameBuilder.java
@@ -0,0 +1,41 @@
+package com.xzh;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * @author 向振华
+ * @date 2022/05/11 16:26
+ */
+public class JFrameBuilder {
+
+    public static void init(Component... components) {
+        init("", 400, 200, components);
+    }
+
+    public static void init(String title, int width, int height, Component... components) {
+        // 面板
+        JPanel panel = new JPanel();
+        for (Component component : components) {
+            panel.add(component);
+        }
+
+        // 窗口
+        JFrame frame = new JFrame();
+        // 设置窗口标题
+        frame.setTitle(title);
+        // 设置窗口显示尺寸
+        frame.setSize(width, height);
+        // 设置窗口是否可以调整大小
+        frame.setResizable(false);
+        // 设置窗口是否可见
+        frame.setVisible(true);
+        // 设置窗口是否可以关闭
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // 设置窗口居中
+        Dimension dimension = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation((dimension.width - frame.getWidth()) / 2, (dimension.height - frame.getHeight()) / 2);
+        // 将面板添加到窗口
+        frame.add(panel);
+    }
+}
