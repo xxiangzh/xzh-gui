c872fd330788c71bc27f162fbdc524fd3297f24b
xzh <anenan@qq.com>
2022-05-11 17:04:56 +0800

    文件操作

--- src/main/java/com.xzh/Application.java
+++ src/main/java/com.xzh/Application.java
@@ -2,6 +2,9 @@ package com.xzh;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
 /**
  * @author 向振华
@@ -11,17 +14,53 @@ public class Application {
 
     public static void main(String[] args) {
 
-        // 标签
-        JLabel label = new JLabel();
-        label.setText("标签");
+        JLabel label = new JLabel("选择文件：");
 
-        // 单行文本框
         JTextField textField = new JTextField();
         textField.setPreferredSize(new Dimension(200, 20));
 
-        // 按钮
-        JButton button = new JButton("按钮");
+        JButton button1 = new JButton("浏览");
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
 
-        JFrameBuilder.init(label, textField, button);
+                JFileChooser fileChooser = new JFileChooser(".");
+                fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                fileChooser.showDialog(new JLabel(), "选择文件");
+
+                File sourceFile = fileChooser.getSelectedFile();
+                if (sourceFile != null && sourceFile.isFile()) {
+                    textField.setText(sourceFile.getAbsolutePath());
+                }
+            }
+        });
+
+        JButton button2 = new JButton("执行");
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                File sourceFile = new File(textField.getText());
+                // TODO
+            }
+        });
+
+        JButton button3 = new JButton("下载");
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+
+                JFileChooser fileChooser = new JFileChooser(".");
+                fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                fileChooser.showDialog(new JLabel(), "选择路径");
+
+                File targetDirectory = fileChooser.getSelectedFile();
+                if (targetDirectory != null && targetDirectory.isDirectory()) {
+                    String targetDirectoryAbsolutePath = targetDirectory.getAbsolutePath();
+                    // TODO
+                }
+            }
+        });
+
+        JFrameBuilder.init(label, textField, button1, button2, button3);
     }
 }
